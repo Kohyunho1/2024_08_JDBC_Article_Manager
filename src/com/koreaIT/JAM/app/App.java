f367904ab689a5a38436676b38d3b5e81b7da233
Kohyunho1 <zigzag7489@gmail.com>
2024-08-09 23:00:25 +0900

    권한체크, DateTimeFormatter

--- src/com/koreaIT/JAM/app/App.java
+++ src/com/koreaIT/JAM/app/App.java
@@ -12,67 +12,69 @@ public class App {
 	private final String url = "jdbc:mysql://localhost:3306/2024_08_JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 	private final String username = "root";
 	private final String password = "";
-	
+
 	public void run() {
 		System.out.println("== 프로그램 시작 ==");
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-        Connection conn = null;
-        
-        try {
-        	conn = DriverManager.getConnection(url, username, password);
-        	
-        	MemberController memberController = new MemberController(conn, sc);
-            ArticleController articleController = new ArticleController(conn, sc);
-        	
-    		while (true) {
-    			System.out.printf("명령어) ");
-    			String cmd = sc.nextLine().trim();
-    			
-    			if (cmd.equals("exit")) {
-    				break;
-    			}
-    			
-    			if (cmd.length() == 0) {
-    				System.out.println("명령어를 입력해주세요");
-    				continue;
-    			}
-    			
-    			if (cmd.equals("member join")) {
-    				memberController.doJoin();
-    			} else if (cmd.equals("member login")) {
-    				memberController.doLogin();
-    			} else if (cmd.equals("article write")) {
-    				articleController.doWrite();
-    			} else if (cmd.equals("article list")) {
-    		        articleController.showList();
-    			} else if (cmd.startsWith("article detail ")) {
-    				articleController.showDetail(cmd);
-    			} else if (cmd.startsWith("article modify ")) {
-    				articleController.doModify(cmd);
-    			} else if (cmd.startsWith("article delete ")) {
-    				articleController.doDelete(cmd);
-    			} else {
-    				System.out.println("존재하지 않는 명령어 입니다");
-    			}
-    		}
-    		sc.close();
-    		
+
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+
+			MemberController memberController = new MemberController(conn, sc);
+			ArticleController articleController = new ArticleController(conn, sc);
+
+			while (true) {
+				System.out.printf("명령어) ");
+				String cmd = sc.nextLine().trim();
+
+				if (cmd.equals("exit")) {
+					break;
+				}
+
+				if (cmd.length() == 0) {
+					System.out.println("명령어를 입력해주세요");
+					continue;
+				}
+
+				if (cmd.equals("member join")) {
+					memberController.doJoin();
+				} else if (cmd.equals("member login")) {
+					memberController.doLogin();
+				} else if (cmd.equals("member logout")) {
+					memberController.doLogout();
+				} else if (cmd.equals("article write")) {
+					articleController.doWrite();
+				} else if (cmd.equals("article list")) {
+					articleController.showList();
+				} else if (cmd.startsWith("article detail ")) {
+					articleController.showDetail(cmd);
+				} else if (cmd.startsWith("article modify ")) {
+					articleController.doModify(cmd);
+				} else if (cmd.startsWith("article delete ")) {
+					articleController.doDelete(cmd);
+				} else {
+					System.out.println("존재하지 않는 명령어 입니다");
+				}
+			}
+			sc.close();
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		
+
 		System.out.println("== 프로그램 끝 ==");
 	}
 }
--- src/com/koreaIT/JAM/dao/ArticleDao.java
+++ src/com/koreaIT/JAM/dao/ArticleDao.java
@@ -15,44 +15,45 @@ public class ArticleDao {
 		this.conn = conn;
 	}
 
-	public void writeArticle(String title, String body) {
+	public int writeArticle(int loginedMemberId, String title, String body) {
 		SecSql sql = new SecSql();
 		sql.append("INSERT INTO article");
 		sql.append("SET regDate = NOW()");
 		sql.append(", updateDate = NOW()");
+		sql.append(", memberId = ?", loginedMemberId);
 		sql.append(", title = ?", title);
 		sql.append(", `body` = ?", body);
 		
-		DBUtil.insert(conn, sql);
-	}
-	
-	public int getLastInsertId() {
-		SecSql sql = new SecSql();
-		sql.append("SELECT LAST_INSERT_ID()");
-		return 0;
+		return DBUtil.insert(conn, sql);
 	}
 
 	public List<Map<String, Object>> getArticles() {
 		SecSql sql = new SecSql();
-		sql.append("SELECT * FROM article");
-		sql.append("ORDER BY id DESC");
+		sql.append("SELECT a.*, m.loginId AS writerName");
+		sql.append("FROM article AS a");
+		sql.append("INNER JOIN `member` AS m");
+		sql.append("ON a.memberId = m.id");
+		sql.append("ORDER BY a.id DESC");
 		
 		return DBUtil.selectRows(conn, sql);
 	}
 
 	public Map<String, Object> getArticle(int id) {
-		SecSql sql = SecSql.from("SELECT * FROM article");
-		sql.append("WHERE id = ?", id);
+		SecSql sql = SecSql.from("SELECT a.*, m.loginId AS writerName");
+		sql.append("FROM article AS a");
+		sql.append("INNER JOIN `member` AS m");
+		sql.append("ON a.memberId = m.id");
+		sql.append("WHERE a.id = ?", id);
 		
 		return DBUtil.selectRow(conn, sql);
 	}
 
-	public int getArticleCnt(int id) {
-		SecSql sql = new SecSql();
-        sql.append("SELECT COUNT(id) FROM article");
-        sql.append("WHERE id = ?", id);
-        
-		return DBUtil.selectRowIntValue(conn, sql);
+	public Map<String, Object> getArticleById(int id) {
+		SecSql sql = SecSql.from("SELECT *");
+		sql.append("FROM article");
+		sql.append("WHERE id = ?", id);
+		
+		return DBUtil.selectRow(conn, sql);
 	}
 
 	public void modifyArticle(int id, String title, String body) {
@@ -64,14 +65,6 @@ public class ArticleDao {
         sql.append("WHERE id = ?", id);
 
         DBUtil.update(conn, sql);
-	}
-
-	public boolean isExists(int id) {
-		SecSql sql = new SecSql();
-        sql.append("SELECT COUNT(id) > 0 FROM article");
-        sql.append("WHERE id = ?", id);
-        
-		return DBUtil.selectRowBooleanValue(conn, sql);
 	}
 
 	public void deleteArticle(int id) {
